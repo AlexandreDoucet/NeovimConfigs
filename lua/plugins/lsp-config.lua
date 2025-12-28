73501fa0ad1fd128576d7b8a8d9447cee0260a3a
Alexandre Doucet <alexandre.doucet6@gmail.com>
2025-12-28 13:40:32 -0400

    Updated to neovim 0.11

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,62 +1,88 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
 
-			lspconfig.lua_ls.setup({})
-			lspconfig.rust_analyzer.setup({})
-			--lspconfig.jedi_language_server.setup({})
-			lspconfig.clangd.setup({})
-			lspconfig.eslint.setup({})
-			lspconfig.jinja_lsp.setup({})
-			lspconfig.cssls.setup({})
+    config = function()
+      ------------------------------------------------------------------
+      -- LSP server definitions (Neovim 0.11 native)
+      ------------------------------------------------------------------
 
-			-- Diagnostic keymaps
-			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+      vim.lsp.config.lua_ls = {}
+      vim.lsp.config.rust_analyzer = {}
+      vim.lsp.config.clangd = {}
+      vim.lsp.config.eslint = {}
+      vim.lsp.config.jinja_lsp = {}
+      vim.lsp.config.cssls = {}
 
-			-- LspAttach autocommand
+      -- Enable servers
+      vim.lsp.enable({
+        "lua_ls",
+        "rust_analyzer",
+        "clangd",
+        "eslint",
+        "jinja_lsp",
+        "cssls",
+      })
 
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					-- Check if the buffer is valid
-					if not ev.buf or not vim.api.nvim_buf_is_valid(ev.buf) then
-						return
-					end
+      ------------------------------------------------------------------
+      -- Global diagnostic keymaps
+      ------------------------------------------------------------------
 
-					-- Enable completion triggered by <c-x><c-o>
-					--vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+      vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
-					-- Buffer-local mappings
-					local opts = { buffer = ev.buf }
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-					vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-					vim.keymap.set("n", "<space>wl", function()
-						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-					end, opts)
-					vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-					vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
-					vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					vim.keymap.set("n", "<space>f", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
+      ------------------------------------------------------------------
+      -- LspAttach: buffer-local mappings
+      ------------------------------------------------------------------
 
-					vim.keymap.set("n", "<leader>gw", vim.diagnostic.goto_next, { desc = "Go to Next Diagnostic" })
-					vim.keymap.set("n", "<leader>ge", function()
-						vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
-					end, { desc = "Go to Next Error" })
-				end,
-			})
-		end,
-	},
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+        callback = function(ev)
+          if not ev.buf or not vim.api.nvim_buf_is_valid(ev.buf) then
+            return
+          end
+
+          local opts = { buffer = ev.buf }
+
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+
+          vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set("n", "<space>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, opts)
+
+          vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+          vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+          vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+
+          vim.keymap.set("n", "<space>f", function()
+            vim.lsp.buf.format({ async = true })
+          end, opts)
+
+          vim.keymap.set("n", "<leader>gw", vim.diagnostic.goto_next, {
+            buffer = ev.buf,
+            desc = "Go to Next Diagnostic",
+          })
+
+          vim.keymap.set("n", "<leader>ge", function()
+            vim.diagnostic.goto_next({
+              severity = vim.diagnostic.severity.ERROR,
+            })
+          end, {
+            buffer = ev.buf,
+            desc = "Go to Next Error",
+          })
+        end,
+      })
+    end,
+  },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,25 +2,7 @@
 --sudo chown adoucet:users ~/.local/share/nvim/lazy/nvim-treesitter/parser
 --
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	enabled = true,
-
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			auto_install = true,
-			ensure_installed = { "c", "lua", "html", "css", "javascript", "python" },
-			sync_install = false,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = { "jinja" },
-			},
-			indent = { enable = true },
-
-			completion = {
-				enable = true, -- Enable completion source from Treesitter
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ':TSUpdate',
 }
